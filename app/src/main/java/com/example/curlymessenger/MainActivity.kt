08c4df8fe44a439d2454ca3145f80f392286c9ec
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-12-20 19:39:33 +0100

    Small bugfix.

--- app/src/main/java/com/example/curlymessenger/MainActivity.kt
+++ app/src/main/java/com/example/curlymessenger/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.curlymessenger
 
 import android.os.Bundle
-import android.renderscript.ScriptGroup.Binding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
