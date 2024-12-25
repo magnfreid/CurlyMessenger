db16e64e3ee68f52dd2c24df6cf7fc15c98a4ad7
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-12-25 22:34:27 +0100

    Added auth repository.

--- app/src/main/java/com/example/curlymessenger/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/curlymessenger/viewmodel/AuthViewModel.kt
@@ -1,26 +1,38 @@
 package com.example.curlymessenger.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.Firebase
+import com.example.curlymessenger.repository.AuthRepository
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.auth
 
 class AuthViewModel : ViewModel() {
     private val authRepository = AuthRepository()
 
     val currentUser: LiveData<FirebaseUser> get() = authRepository.currentUser
 
-    fun signInWithEmailAndPassword(email: String, password: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+    fun signInWithEmailAndPassword(
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
         authRepository.signInWithEmailAndPassword(email, password, onSuccess, onFailure)
     }
 
-    fun registerWithEmailAndPassword(email: String, password: String, onSuccess: (FirebaseUser) -> Unit, onFailure: (Exception) -> Unit){
+    fun registerWithEmailAndPassword(
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
         authRepository.registerWithEmailAndPassword(email, password, onSuccess, onFailure)
     }
 
-    fun loginWithGoogleAccount(email:String) {
-        //TODO implement Google sign in
+    fun loginWithGoogleAccount(
+        email: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        authRepository.signInWithGoogleAccount(email, onSuccess, onFailure)
     }
 }
