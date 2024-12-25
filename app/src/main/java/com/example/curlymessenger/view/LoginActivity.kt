d056bbfac57fde3ebf9076ecdff97ae166ca9c02
yousserflaamim <yousserflamin99@gmail.com>
2024-12-25 12:06:40 +0100

    updet activitylogin

--- app/src/main/java/com/example/curlymessenger/view/LoginActivity.kt
+++ app/src/main/java/com/example/curlymessenger/view/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.curlymessenger.view
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,27 @@ import com.example.curlymessenger.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    lateinit var prg :ProgressBar
+    lateinit var email:String
+    lateinit var pass:String
+   // lateinit private var firauth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding=ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        //add logic fairbase
+
+//btn login
+
+        binding.btnlogin.setOnClickListener{
+
+        }
+
+    }
+//logic singin
+    private fun signIn(){
 
     }
 }
