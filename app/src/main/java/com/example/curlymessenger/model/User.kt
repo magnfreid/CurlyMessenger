b64ce9f7d6da932d7cf0bbc1bc1d44050ee4e746
Jonas N <jonasniyazson@hotmail.com>
2025-01-02 09:21:40 +0100

    Added avatar

--- app/src/main/java/com/example/curlymessenger/model/User.kt
+++ app/src/main/java/com/example/curlymessenger/model/User.kt
@@ -2,4 +2,6 @@ package com.example.curlymessenger.model
 
 data class User(val id: String,
                 val nickname: String? = null,
-                val email: String? = null)
+                val email: String? = null,
+                val avatar: String? = null
+                )
