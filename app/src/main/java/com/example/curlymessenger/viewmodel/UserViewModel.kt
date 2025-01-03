dbf65cc3e946665d7f61b089247e2b797b10b5eb
Jonas N <jonasniyazson@hotmail.com>
2025-01-03 18:13:22 +0100

    Added startNewChat

--- app/src/main/java/com/example/curlymessenger/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/curlymessenger/viewmodel/UserViewModel.kt
@@ -3,13 +3,19 @@ package com.example.curlymessenger.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.curlymessenger.model.User
+import com.example.curlymessenger.repository.ChatRepository
 import com.example.curlymessenger.repository.UserRepository
 
 class UserViewModel : ViewModel() {
 
     private val userRepo = UserRepository()
+    private val chatRepo = ChatRepository()
 
     val allUsers: LiveData<List<User>> get() = userRepo.users
     val currentUser: LiveData<User?> get() = userRepo.currentUser
+
+    fun startNewChat(participants: List<User>, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        chatRepo.startNewChat(participants, onSuccess, onFailure)
+    }
 
 }
